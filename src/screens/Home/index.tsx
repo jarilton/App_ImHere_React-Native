bd1c81d70cb07f42a8622fc348a41cf42fd43bd5
Jarilton <jariltonpereira02@hotmail.com>
2022-08-13 00:14:35 -0300

    Input de dados

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, View, TextInput } from 'react-native';
 import { styles } from './styles';
 
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>APP ImHere React Native </Text>
+      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventDate}>Sábado, 13 de Agosto de 2022</Text>
+
+      <TextInput 
+        style={styles.input}
+        placeholder="Nome do participante..."
+        placeholderTextColor="#ffffff"
+      />
+      
       <StatusBar style="auto" />
     </View>
   );
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -7,7 +7,19 @@ export const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
-    text: {
+    eventName: {
+        fontSize: 24,
+    },
+    eventDate: {
         fontSize: 18,
+    },
+    input: {
+        height: 56,
+        width: 350, 
+        backgroundColor: '#1f1e25',
+        color: '#ffffff', 
+        borderRadius: 5,
+        padding: 16,
+        fontSize: 16,
     }
 });
